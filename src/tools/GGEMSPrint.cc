ec9ee5ad983202ce305c162df62f0dab8faedb27
Didier Benoit <didier.benoit13@protonmail.com>
2025-10-06 12:39:14 +0200

    ggems v2 and pybind11

---
+++ src/tools/GGEMSPrint.cc
@@ -0,0 +1,32 @@
+// ************************************************************************
+// * This file is part of GGEMS.                                          *
+// *                                                                      *
+// * GGEMS is free software: you can redistribute it and/or modify        *
+// * it under the terms of the GNU General Public License as published by *
+// * the Free Software Foundation, either version 3 of the License, or    *
+// * (at your option) any later version.                                  *
+// *                                                                      *
+// * GGEMS is distributed in the hope that it will be useful,             *
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+// * GNU General Public License for more details.                         *
+// *                                                                      *
+// * You should have received a copy of the GNU General Public License    *
+// * along with GGEMS.  If not, see <https://www.gnu.org/licenses/>.      *
+// *                                                                      *
+// ************************************************************************
+
+#include "GGEMS/tools/GGEMSPrint.hh"
+
+Pet::Pet(std::string const& name)
+: name_(name) {
+  ;
+}
+
+void Pet::SetName(std::string const& name) {
+  name_ = name;
+}
+
+std::string const& GetName(void) const {
+  return name_;
+}
